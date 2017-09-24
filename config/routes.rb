beff4568a91da904a0d13d26517e837903942fdf
nurlanemir <nurlan.emiroff@gmail.com>
2017-09-24 19:54:16 +0200

    Comment model generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'landing/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   get '/articles/:id', to: 'articles#show'
   root controller: :landing, action: :index
 end
